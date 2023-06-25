014cec0251e053135c184b17a75f6cf34ece7657
faiyaj01 <faiyajahmed710@gmail.com>
2023-06-26 01:58:47 +0600

    Fixed the input query issue

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 let response = [];
+let input = document.querySelector("#emoji_search");
+
 const emoji_api = async () => {
   data = await fetch(
     "https://emoji-api.com/emojis?access_key=6395e0fc0e783f8788349bc1bfa6f23ac4358c2e"
   );
   data = await data.json();
-  // console.log(data);
-  // data.map((item) => response.push(item));
   return data;
 };
-let input = document.querySelector("#emoji_search");
 
-const formList = async () => {
+const apiToList = async () => {
   response = await emoji_api();
+};
+apiToList();
+
+const formList = async (list) => {
   let font = `<i class="fa-solid fa-clipboard fa-bounce"></i>`;
   let ul = document.querySelector("#list");
-
-  console.log(response);
-  for (let i of response.slice(0, 10)) {
-    console.log("i");
+  console.log(list);
+  // console.log(response);
+  for (let i of list) {
+    // console.log("i");
     let li = document.createElement("li");
     let button = document.createElement("button");
     let div = document.createElement("div");
@@ -39,12 +42,20 @@ const formList = async () => {
 };
 
 const inputQueries = () => {
+  let listOfFiltered = [];
   filter = input.value.toLowerCase();
-  let p = document.getElementsByTagName("p");
-  console.log(p);
-
   console.log(filter);
-  for (let i of p) {
+  let ul = document.getElementsByTagName("ul");
+  if (ul[0].children.length > 0) {
+    Array.from(ul[0].children).map((item) => item.remove());
   }
+  for (let i of response) {
+    a = i.slug;
+    if (a.includes(filter)) {
+      listOfFiltered.push(i);
+    }
+  }
+  // console.log(list);
+  formList(listOfFiltered);
 };
-formList();
+// formList();
