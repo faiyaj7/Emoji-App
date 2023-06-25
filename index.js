76b86ae8cd31fcc52b33cffcf519730d37e96d1c
faiyaj01 <faiyajahmed710@gmail.com>
2023-06-26 00:13:37 +0600

    Changed the structure

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-window.addEventListener("load", async () => {
-  let response = await fetch(
+let response = [];
+const emoji_api = async () => {
+  data = await fetch(
     "https://emoji-api.com/emojis?access_key=6395e0fc0e783f8788349bc1bfa6f23ac4358c2e"
   );
-  response = await response.json();
-  console.log(response);
-  let font = `<i class="fa-solid fa-clipboard fa-bounce"></i>;`;
-  let input = document.querySelector("#emoji_search");
+  data = await data.json();
+  // console.log(data);
+  // data.map((item) => response.push(item));
+  return data;
+};
+let input = document.querySelector("#emoji_search");
+
+const formList = async () => {
+  response = await emoji_api();
+  let font = `<i class="fa-solid fa-clipboard fa-bounce"></i>`;
   let ul = document.querySelector("#list");
-  for (let i of response[10]) {
+
+  console.log(response);
+  for (let i of response.slice(0, 10)) {
+    console.log("i");
     let li = document.createElement("li");
     let button = document.createElement("button");
     let div = document.createElement("div");
-    div.setAttribute("class", "single_row");
+    let p = document.createElement("p");
+    p.textContent = div.setAttribute("class", "single_row");
+
+    p.textContent = i.slug;
     li.textContent = i.character;
     button.innerHTML = font;
-    div.append(li, button);
+
+    // copying the emoji
+    button.addEventListener("click", () => {
+      navigator.clipboard.writeText(i.character);
+    });
+
+    div.append(li, p, button);
     ul.append(div);
   }
-});
+};
+
+const inputQueries = () => {
+  filter = input.value.toLowerCase();
+  let p = document.getElementsByTagName("p");
+  console.log(p);
+
+  console.log(filter);
+  for (let i of p) {
+  }
+};
+formList();
